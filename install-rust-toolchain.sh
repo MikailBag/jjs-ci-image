d3f71577981637b040f26a2d87812b3b15562674
Mikail Bagishov <bagishov.mikail@yandex.ru>
2019-08-06 17:39:49 +0300

    More verbose progress

--- install-rust-toolchain.sh
+++ install-rust-toolchain.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-
-rustup default nightly-2019-07-03
+VERSION="2019-07-30"
+echo Installing Rust nightly release ${VERSION}
+rustup default nightly-${VERSION}
+echo Installing additional components
 rustup component add clippy rustfmt
+echo Installing musl target support
 rustup target add x86_64-unknown-linux-musl
+echo Installing additional tools
 cargo install just mdbook
