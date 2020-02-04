66b4163f5dc6b7facbc0ebe0195ed1243ebcc978
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-02-04 21:00:46 +0300

    Fix

--- install-rust-toolchain.sh
+++ install-rust-toolchain.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain none
+source $HOME/.cargo/env
 rustup toolchain install nightly-2020-01-31 --component clippy --component rustfmt
 rustup default nightly
